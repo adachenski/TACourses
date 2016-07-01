2431f80fb4163e30f4ce20dc92b79a4bebfe31c8
adachenski <adachenski@aol.com>
2016-07-01 01:52:33 -0700

    mongoose configuration added to the server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express'),
     stylus = require('stylus'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 var env = process.env.NODE_ENV || 'development';
 
 var app = express();
@@ -21,6 +22,46 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname+'/public'));
 
+mongoose.connect('mongodb://localhost/telerikdb');
+
+var db = mongoose.connection;
+
+db.once('once',function(err){
+
+    if(err){
+        console.log('Database error:'+err);
+        return;
+    }
+    console.log('database is up and running!')
+});
+
+db.on('error',function(err){
+    console.log(err);
+});
+
+var messageSchema = mongoose.Schema({
+    message:String
+});
+
+var Message = mongoose.model('Message',messageSchema);
+var messageFromDataBase;
+
+Message.remove().exec(function(err){
+
+    if(err){
+        console.log('Message could not be cleared '+err);
+        return;
+    }
+
+    console.log('Message deleted!');
+    Message.create({message:'Hi from mongoose!'})
+        .then(function(model){
+        messageFromDataBase = model.message;
+    });
+})
+
+
+
 app.get('/partials/:partialName',function(req,res){
 
     res.render('partials/'+req.params.partialName)
@@ -28,7 +69,7 @@ app.get('/partials/:partialName',function(req,res){
 
 app.get('*',function(req,res){
 
-    res.render('index');
+    res.render('index', {message:messageFromDataBase});
 });
 
 app.listen(port);
