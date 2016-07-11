ee67cad307c7b342a4c11e1f02b0ea0628027292
adachenski <adachenski@aol.com>
2016-07-11 04:35:53 -0700

    authentication logic moved to other file.

--- public/app/account/auth.js
+++ public/app/account/auth.js
@@ -2,7 +2,7 @@
  * Created by Just Nasko on 7/10/2016.
  */
 
-app.factory('auth',function($q){
+app.factory('auth',function($q,$http, identity){
 
     return{
         login:function(user){
@@ -15,13 +15,12 @@ app.factory('auth',function($q){
 
                     deffer.resolve(true);
                     identity.currentUser = response.user;
-                    notifier.success("Login Success");
                 }
                 else{
                     deffer.resolve(false);
-                    notifier.error("Wrong username or password");
                 }
             });
+            return deffer.promise;
         }
     }
 });
--- public/app/account/login-controller.js
+++ public/app/account/login-controller.js
@@ -1,11 +1,19 @@
 /**
  * Created by Just Nasko on 7/4/2016.
  */
-app.controller('LoginCtrl',function($scope, $http,notifier,identity,auth){
+app.controller('LoginCtrl',function($scope,notifier,identity,auth){
 
     $scope.identity = identity;
     $scope.login=function(user){
 
+        auth.login(user).then(function(success){
 
+            if(success){
+                notifier.success('Successful login');
+            }
+            else{
+                notifier.error('Username or Password not valid')
+            }
+        });
     }
 });
